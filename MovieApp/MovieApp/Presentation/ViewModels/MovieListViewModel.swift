29ddb3fe737caefc19cc6b2737a09b6a6be3eb56
Ricardo Montesinos <6757869+richimf@users.noreply.github.com>
2025-02-23 21:46:16 -0600

    [API-002] Fetch movie list from API

--- MovieApp/MovieApp/Presentation/ViewModels/MovieListViewModel.swift
+++ MovieApp/MovieApp/Presentation/ViewModels/MovieListViewModel.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 class MovieListViewModel: ObservableObject {
- 
+
+    @Published var movies: [Movie] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+
     private let fetchMoviesUseCase: FetchMoviesUseCase
 
     init(fetchMoviesUseCase: FetchMoviesUseCase) {
@@ -17,6 +21,19 @@ class MovieListViewModel: ObservableObject {
     }
 
     func fetchMovies() {
+        isLoading = true
+        errorMessage = nil
 
+        fetchMoviesUseCase.execute(page: 1, language: .EN) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result {
+                case .success(let movies):
+                    self?.movies = movies
+                case .failure(let error):
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
     }
 }
